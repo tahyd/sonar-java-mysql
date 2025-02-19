e54fbeb8dc27ec0972341725eed413835e6ba34e
tahyd <ta.hyd.003@gmail.com>
2025-02-19 12:52:09 +0530

    code base modified2

--- src/main/java/com/devops/mysql/EmployeeController.java
+++ src/main/java/com/devops/mysql/EmployeeController.java
@@ -1,12 +1,13 @@
 package com.devops.mysql;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
 	@GetMapping("/hello/{name}")
-	public String index() {
-		return "Hello World ..... !";
+	public String index(@PathVariable("name") String name) {
+		return "Hello World ..... !"+name;
 	}
 
 }
